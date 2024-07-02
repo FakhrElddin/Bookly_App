9cf978e5f00826d41db93b009ec5d6232ccaad59
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-02 08:16:28 +0300

    create book model with equatable

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,6 +1,7 @@
-import 'package:bookly/Features/home/data/models/BookModel.dart';
 import 'package:bookly/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
+
+import '../models/book_model/book_model.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
