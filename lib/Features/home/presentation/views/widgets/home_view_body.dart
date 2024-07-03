2d1e65a0daa38bbc67b2d355a04983d6c83df41c
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-03 15:15:03 +0300

    add shimmer package to pubspec and rename best seller list view

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,9 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
-import 'best_seller_list_view.dart';
 import 'custom_app_bar.dart';
 import 'featured_books_list_view.dart';
+import 'newest_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -36,7 +36,7 @@ class HomeViewBody extends StatelessWidget {
           ),
         ),
         SliverFillRemaining(
-          child: BestSellerListView(),
+          child: NewestBooksListView(),
         ),
         SliverToBoxAdapter(
           child: SizedBox(
--- lib/Features/home/presentation/views/widgets/newest_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/newest_books_list_view.dart
@@ -6,8 +6,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'book_list_view_item.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class NewestBooksListView extends StatelessWidget {
+  const NewestBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
